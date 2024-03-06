ab5a768817ee1830f38928b590c5174a9d5f635b
Anna Moon <anna@fourmoonstech.com>
2024-03-06 12:17:21 -0600

    configured mutations and queries

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,12 +1,24 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
-  mutation login($email: String!, $password: String!) {
+  mutation userLogin($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
       user {
         _id
         username
+      }
+    }
+  }
+`;
+
+export const LOGIN_CHILD_USER = gql`
+  mutation childUserLogin($childUsername: String!, $password: String!) {
+    userLogin(childUsername: $childUsername, password: $password) {
+      token
+      user {
+        _id
+        childUsername
       }
     }
   }
@@ -20,6 +32,40 @@ export const ADD_USER = gql`
         _id
         username
       }
+    }
+  }
+`;
+
+export const ADD_CHILD_USER = gql`
+  mutation addChildUser($childUsername: String!, $password: String!) {
+    addChildUser(childUsername: $childUsername, password: $password) {
+      token
+      user {
+        _id
+        childUsername
+      }
+    }
+  }
+`;
+
+export const ADD_CHORE = gql`
+  mutation addChore($description: String!,$payRate:Number, $dueDate: Date, $child_id: ID ){
+    addChore(description: $description, payRate: $payRate, dueDate: $dueDate, childId:$child_id ){
+      choreId
+      description
+      payRate
+      dueDate
+      child_id
+    }
+  }
+`;
+
+export const COMPLETE_CHORE = gql`
+  mutation completeChore($chore: ID!, $complete: Boolean!) {
+    completeChore(choreId: $chore, complete: $complete) {
+      _id
+      complete
+      
     }
   }
 `;
